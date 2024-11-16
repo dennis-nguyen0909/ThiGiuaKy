de21010b5532d607d497f96b6877af7476cd3d68
Duy <dennis.nguyen0909@gmal.com>
2024-11-16 07:36:52 +0700

    nhap chuoi

---
+++ nhapSoNguyen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cout << "Nhap vao mot so nguyen :" <<endl;
+	cin >> n;
+	cout << "So nguyen vua nhap la : " <<n<<endl;
+	
+	
+	return 0;
+}
