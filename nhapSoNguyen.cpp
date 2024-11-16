417206312f97eeb796dae0aa1f5cc00cbc3a2982
Duy <dennis.nguyen0909@gmal.com>
2024-11-16 07:57:10 +0700

    kiem tra chan

--- nhapSoNguyen.cpp
+++ nhapSoNguyen.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
+bool kiemtraChan(int n){
+	return n%2==0;
+}
 int main(){
 	int n;
 	cout << "Nhap vao mot so nguyen :" <<endl;
 	cin >> n;
 	cout << "So nguyen vua nhap la : " <<n<<endl;
 	
+	int kqChan = kiemtraChan(n);
+	if(kqChan){
+		cout <<"La so chan"<<endl;
+	}else{
+		cout<<"Ko la so chan"<<endl;
+	}
 	
 	return 0;
 }
